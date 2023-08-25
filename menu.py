fe918eac76eae0b0ac59a6595c939d4c91065699
BrenMGarcia <mondragongarciabrenda2000@gmail.com>
2023-08-25 08:00:45 -0600

    Archivos_base_py

---
+++ menu.py
@@ -0,0 +1,32 @@
+import os 
+import sys
+
+from qgis.core import *
+from qgis.gui import *
+from qgis.utils import *
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+
+from .main import main_interfaz
+
+class menuInit:
+    def __init__(self,iface):
+        self.iface = iface
+
+    def initGui(self):
+        self.EMenu = QMenu(self.iface.mainWindow())
+        self.EMenu.setTitle("Ejemplo1")
+        self.EMenuBar = self.iface.mainWindow().menuBar()
+        self.EMenuBar.insertMenu(self.iface.firstRightStandardMenu().menuAction(),self.EMenu)
+        self.EMenuBar = self.iface.addToolBar("Ejemplo")
+
+        self.action = QAction(QIcon(":/Base_plugin/icon.png"),"Ejmplo",self.iface.maimWindow())
+        self.EMenu.addAction(self.action)
+        self.action.trigger.connect(self.dialogo)
+        
+        self.iface.addToolBarIcon(self.action)
+        self.iface.addPluginToMenu("Ejemplo",self.action)
+
+    def dialogo(self):
+        self.diag = main_interfaz()
+        self.diag.show()
